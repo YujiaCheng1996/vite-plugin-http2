df328c488d46fc0cab8d7f17ee928ebf9ef3473a
chengyujia <chengyujia@email.cn>
2022-08-12 09:45:21 +0800

    <feat>prepare npm publish

--- README.md
+++ README.md
@@ -2,54 +2,56 @@
 
 ![vite-http2.jpg](http://tva1.sinaimg.cn/large/005KcNyUly1gzhr1ei0b4j30gj04hjrg.jpg)
 
-> npm i vite-plugin-http2
+> npm i vite-plugin-http2-ws
 
+fork 自 [strongcode9527/vite-plugin-http2](https://github.com/strongcode9527/vite-plugin-http2) ，在其上增加 websocket 支持。
 
-vite-plugin-http2 是一个 vite 插件，是为了解决 [vite 无法同时开启 proxy 代理以及 http2的问题](https://github.com/vitejs/vite/issues/484)
+vite-plugin-http2-ws 是一个 vite 插件，是为了解决 [vite 无法同时开启 proxy 代理以及 http2 的问题](https://github.com/vitejs/vite/issues/484)
 
 这个插件可以解决一下难题
 
 - 使用 devcert 库自动生成 https 证书，并且自动配置在系统内部（第一次进入开发环境，需要输入密码将证书放入系统指定目录）
 - 使用 http2-proxy 进行代理转发。
+- 有限支持 websocket，仅 log error 不能处理。
 
 ```javascript
 // vite.config.js
 
-import http2 from 'vite-plugin-http2';
+import http2 from "vite-plugin-http2-ws";
 
 export default {
-    plugins: [
-        http2 ({
-            // 如果你的开发环境只是用 localhost 开发，这个配置项可以忽略
-            certificateDomain: ['my-test.xxx.com'],
-            proxy: {
-                // 创建正则表达式的字符串，这里识别需要代理的接口
-                '^/api': {
-                    hostname: 'localhost',
-                    port: 7001,
-                    async onReq(req, options) {
-                        // 如果路径需要修改，可以在这里修改添加
-                        options.path = `/prefix/${options.path}`;
-                    }
-                }
-            },
-            // 如果 https 证书创建失败，可以自己创建，并在这里传入
-            ssl: {
-                key: '',
-                cert: '',
-            }
-        }),
-    ]
+  plugins: [
+    http2({
+      // 如果你的开发环境只是用 localhost 开发，这个配置项可以忽略
+      certificateDomain: ["my-test.xxx.com"],
+      proxy: {
+        // 创建正则表达式的字符串，这里识别需要代理的接口
+        "^/api": {
+          hostname: "localhost",
+          port: 7001,
+          async onReq(req, options) {
+            // 如果路径需要修改，可以在这里修改添加
+            options.path = `/prefix/${options.path}`;
+          },
+          // 指明需要ws代理
+          ws: true,
+        },
+      },
+      // 如果 https 证书创建失败，可以自己创建，并在这里传入
+      ssl: {
+        key: "",
+        cert: "",
+      },
+    }),
+  ],
 };
-
 ```
 
-
 | key               | desc                                                                                        | default       |
-| ------------------|---------------------------------------------------------------------------------------------| --------------- |
+| ----------------- | ------------------------------------------------------------------------------------------- | ------------- |
 | proxy             | proxy [http2-proxy options](https://github.com/nxtedition/node-http2-proxy#options)         | -             |
 | certificateDomain | [HTTPS certificate domain name](https://github.com/davewasmer/devcert#multiple-domains-san) | ['localhost'] |
-| ssl               | if devcert create certificate fail，you can pass your ssl option                             | -              | 
+| ssl               | if devcert create certificate fail，you can pass your ssl option                            | -             |
 
 使用过程中 https 证书创建失败，或出现诡异问题，可使用此命令清除证书创建的缓存。
 
--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,107 @@
-import type { Plugin } from 'vite';
-import type { http1WebOptions } from 'http2-proxy';
-const devcert = require('devcert');
+import type { Plugin } from "vite";
+import type { http1WebOptions } from "http2-proxy";
+const devcert = require("devcert");
 
-const http2Proxy = require('http2-proxy');
-
-
+const http2Proxy = require("http2-proxy");
 
 type OptionsTypes = {
-    proxy?: { [key: string]: http1WebOptions } & { ws?: boolean } | undefined,
-    certificateDomain?: string | string[] | undefined,
-    ssl?: {
-        key: string;
-        cert: string;
-    }
-}
+  proxy?: ({ [key: string]: http1WebOptions } & { ws?: boolean }) | undefined;
+  certificateDomain?: string | string[] | undefined;
+  ssl?: {
+    key: string;
+    cert: string;
+  };
+};
 
 export default (options?: OptionsTypes): Plugin => {
-    return {
-        name: 'vite-plugin-http2',
-        config: async () => {
-            if (options?.ssl) {
-                return {
-                    server: {
-                        https: {
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-ignore
-                            key: ssl.key,
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-ignore
-                            cert: ssl.cert,
-                        }
-                    }
-                }
-            }
-            let ssl;
-            // 生成证书必须包含 localhost 所以做一下处理
+  return {
+    name: "vite-plugin-http2",
+    config: async () => {
+      if (options?.ssl) {
+        return {
+          server: {
+            https: {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              key: ssl.key,
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              cert: ssl.cert,
+            },
+          },
+        };
+      }
+      let ssl;
+      // 生成证书必须包含 localhost 所以做一下处理
 
-            try {
-                ssl = await devcert.certificateFor(options?.certificateDomain || ['localhost']);
-            } catch (err) {
-                console.error('vite-plugin-http2', err);
+      try {
+        ssl = await devcert.certificateFor(
+          options?.certificateDomain || ["localhost"]
+        );
+      } catch (err) {
+        console.error("vite-plugin-http2", err);
+      }
+      if (ssl && ssl.cert.toString() && ssl.key.toString()) {
+        return {
+          server: {
+            https: {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              key: ssl.key,
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              cert: ssl.cert,
+            },
+          },
+        };
+      }
+      console.warn(
+        "[vite-plugin-http2-ws]: sorry, devcert create certificate fail, you can pass ssl option to create http2 server"
+      );
+      return {};
+    },
+    configureServer: async (server) => {
+      if (options?.proxy) {
+        server.middlewares.use((req, res, next) => {
+          // 如果有一个配置命中请求，进行转发处理
+          for (const [regexp, proxyOptions] of Object.entries(options.proxy)) {
+            const re = new RegExp(regexp);
+            if (req.url && re.test(req.url)) {
+              http2Proxy.web(
+                req,
+                res,
+                typeof proxyOptions === "object"
+                  ? { ...proxyOptions, ws: undefined }
+                  : proxyOptions,
+                (err) => err && next(err)
+              );
+              return;
             }
-            if (ssl && ssl.cert.toString() && ssl.key.toString()) {
-                return {
-                    server: {
-                        https: {
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-ignore
-                            key: ssl.key,
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-ignore
-                            cert: ssl.cert,
-                        }
-                    }
-                };
+          }
+          // 当没有命中代理的时候，直接丢到下一个中间件
+          next();
+        });
+        server.httpServer.on("upgrade", (req, socket, head) => {
+          // 如果有一个配置命中请求，进行转发处理
+          for (const [regexp, proxyOptions] of Object.entries(options.proxy)) {
+            if (proxyOptions?.ws) {
+              const re = new RegExp(regexp);
+              if (req.url && re.test(req.url)) {
+                http2Proxy.ws(
+                  req,
+                  socket,
+                  head,
+                  typeof proxyOptions === "object"
+                    ? { ...proxyOptions, ws: undefined }
+                    : proxyOptions,
+                  (err) => console.error(err)
+                );
+                return;
+              }
             }
-            console.warn('[vite-plugin-http2]: sorry, devcert create certificate fail, you can pass ssl option to create http2 server');
-            return {};
-        },
-        configureServer: async (server) => {
-            if (options?.proxy) {
-                server.middlewares.use((req, res, next) => {
-                    // 如果有一个配置命中请求，进行转发处理
-                    for (const [regexp, proxyOptions] of Object.entries(options.proxy)) {
-                        const re = new RegExp(regexp);
-                        if (req.url && re.test(req.url)) {
-                            http2Proxy.web(
-                                req,
-                                res,
-                                typeof proxyOptions === 'object' ? {...proxyOptions, ws: undefined} : proxyOptions,
-                                err => err && next(err)
-                            );
-                            return;
-                        }
-                    }
-                    // 当没有命中代理的时候，直接丢到下一个中间件
-                    next();
-                });
-                server.httpServer.on('upgrade', (req, socket, head) => {
-                    // 如果有一个配置命中请求，进行转发处理
-                    for (const [regexp, proxyOptions] of Object.entries(options.proxy)) {
-                      const re = new RegExp(regexp);
-                      if (req.url && re.test(req.url) && proxyOptions?.ws ){
-                        console.log(req.url)
-                          http2Proxy.ws(
-                              req,
-                              socket,
-                              head,
-                              typeof proxyOptions === 'object' ? {...proxyOptions, ws: undefined} : proxyOptions,
-                              err => console.error(err)
-                          );
-                          return;
-                      }
-                    }
-                });
-            }
-        },
-    };
+          }
+        });
+      }
+    },
+  };
 };
